9d0625a337cdf2099bb69f62a889f7c3ab29da4a
Alpesh Kumar <alpesh2010kumar@gmail.com>
2024-03-16 23:46:21 +0000

    rename project to library

--- library/wsgi.py
+++ library/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for bookapp project.
+WSGI config for library project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,7 +11,7 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'bookapp.settings.prod')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'library.settings.prod')
 
 
 application = get_wsgi_application()
--- manage.py
+++ manage.py
@@ -7,7 +7,7 @@ import sys
 def main():
     """Run administrative tasks."""
     os.environ.setdefault('DJANGO_SETTINGS_MODULE',
-                          'bookapp.settings.' + os.environ.get('DJANGO_ENV', 'dev'))
+                          'library.settings.' + os.environ.get('DJANGO_ENV', 'dev'))
 
     try:
         from django.core.management import execute_from_command_line
